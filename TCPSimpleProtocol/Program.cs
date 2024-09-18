43cd5ed8315b6f24061ab716f4c5b2bc6ea54aff
Jeppe August Hirsch Jeppsson <jeppe.jeppsson@live.dk>
2024-09-18 11:46:14 +0200

    Add project files.

---
+++ TCPSimpleProtocol/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("write \"s\" for server, write \"c\" for client");
+
+bool isServer = false;
+int port = 20020;
+while (true)
+{
+    string serverOrClient = Console.ReadLine();
+    if (serverOrClient[0] == 's')
+    {
+        isServer = true;
+        break;
+    }
+    if (serverOrClient[0] == 'c')
+    {
+        isServer = false;
+        break;
+    }
+}
+if (isServer)
+    StartServer(port);
+else
+    StartClient(port);
+
+
+
+
+
+void StartServer(int port)
+{
+
+}
+
+void StartClient(int port)
+{
+
+}
