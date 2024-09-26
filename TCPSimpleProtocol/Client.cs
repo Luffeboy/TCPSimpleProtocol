edefe47cc50e88a98288ed5d9719808d819b389b
Jeppe August Hirsch Jeppsson <jeppe.jeppsson@live.dk>
2024-09-26 08:56:03 +0200

    Organization

---
+++ TCPSimpleProtocol/Client.cs
@@ -0,0 +1,77 @@
+﻿using System.Net.Sockets;
+
+public class Client
+{
+    public static void StartClient(int port)
+    {
+        using TcpClient socket = ConnectToServer(port);
+        NetworkStream stream = socket.GetStream();
+        StreamReader reader = new StreamReader(stream);
+        StreamWriter writer = new StreamWriter(stream);
+        while (true)
+        {
+            // user input
+            Console.WriteLine("What function do you want to use? (Random, Add or Subtract)");
+            string method = Console.ReadLine();
+
+            writer.WriteLine(method);
+            writer.Flush();
+            Console.WriteLine(reader.ReadLine());
+            string firstNum = Console.ReadLine();
+            if (firstNum.Contains(" "))
+                writer.WriteLine(firstNum); // if the person wrote "num num" (hopefully)
+            else
+                writer.WriteLine(firstNum + " " + Console.ReadLine());  // if the person wrote "num\nnum"
+            writer.Flush();
+            Console.WriteLine(reader.ReadLine());
+
+
+        }
+    }
+    public static void StartClientJSON(int port)
+    {
+        using TcpClient socket = ConnectToServer(port);
+        NetworkStream stream = socket.GetStream();
+        StreamReader reader = new StreamReader(stream);
+        StreamWriter writer = new StreamWriter(stream);
+        while (true)
+        {
+            // user input
+            Console.WriteLine("What function do you want to use? (Random, Add or Subtract)");
+            string method = Console.ReadLine();
+            Console.WriteLine("input numbers");
+            string firstNum = Console.ReadLine();
+            int firstNumAsInt;
+            int secondNumAsInt;
+            if (firstNum.Contains(' '))
+            {
+                // they wrote the two numbers in one message
+                string[] nums = firstNum.Split(' ');
+                if (nums.Length != 2 || !int.TryParse(nums[0], out firstNumAsInt) || !int.TryParse(nums[1], out secondNumAsInt))
+                    continue; // couldn't parse the numbers or there were too many numbers/words :(
+            }
+            else
+            {
+                // they only wrote one of the numbers
+                if (!int.TryParse(firstNum, out firstNumAsInt) || !int.TryParse(Console.ReadLine(), out secondNumAsInt))
+                    continue; // unable to parse the numbers
+            }
+            writer.WriteLine(new JSONObject(method, firstNumAsInt, secondNumAsInt).ToString());
+            writer.Flush();
+            Console.WriteLine(reader.ReadLine());
+
+
+        }
+    }
+
+    private static TcpClient ConnectToServer(int port)
+    {
+        // get ip address
+        Console.WriteLine("Write ipadress (leave blank for localhost)");
+        string ipAddress = Console.ReadLine();
+        if (string.IsNullOrEmpty(ipAddress))
+            ipAddress = "127.0.0.1";
+        // create connection
+        return new TcpClient(ipAddress, port);
+    }
+}
---
+++ TCPSimpleProtocol/Server.cs
@@ -0,0 +1,103 @@
+﻿using System.Net.Sockets;
+using System.Net;
+
+public class Server
+{
+    public static void StartServer(int port, bool isUsingJSON)
+    {
+        using TcpListener listener = new TcpListener(IPAddress.Any, port);
+        listener.Start();
+        while (true)
+        {
+            TcpClient socket = listener.AcceptTcpClient();
+            if (isUsingJSON)
+                Task.Run(() => { ServerAcceptClientJSON(socket); });
+            else
+                Task.Run(() => { ServerAcceptClient(socket); });
+        }
+        //listener.Dispose(); // it should automatically dispose, due to "using" keyword :)
+    }
+    public static void ServerAcceptClient(TcpClient socket)
+    {
+        NetworkStream stream = socket.GetStream();
+        StreamReader reader = new StreamReader(stream);
+        StreamWriter writer = new StreamWriter(stream);
+        while (true)
+        {
+            string cmd = "";
+            string numbers = "";
+            try
+            {
+                cmd = reader.ReadLine();
+                writer.WriteLine("Input numbers");
+                writer.Flush();
+                numbers = reader.ReadLine();
+            }
+            catch // something messed up
+            {
+                break;
+            }
+            string[] numbersArray = numbers.Split(' ');
+            if (numbersArray.Length != 2)
+                break; // they messed up the message...
+            int firstNum;
+            int secondNum;
+            if (!int.TryParse(numbersArray[0], out firstNum) || !int.TryParse(numbersArray[1], out secondNum))
+                break; // couldn't parse the numbers...
+            string response = ServerMethodResponse(cmd, firstNum, secondNum);
+            writer.WriteLine(response);
+            writer.Flush();
+        }
+        socket.Close();
+    }
+    static void ServerAcceptClientJSON(TcpClient socket)
+    {
+        NetworkStream stream = socket.GetStream();
+        StreamReader reader = new StreamReader(stream);
+        StreamWriter writer = new StreamWriter(stream);
+        while (true)
+        {
+            string cmd = "";
+            JSONObject jsonObject;
+            try
+            {
+                cmd = reader.ReadLine();
+                jsonObject = JSONObject.Deserialize(cmd);
+            }
+            catch // something messed up
+            {
+                break;
+            }
+
+            string response = ServerMethodResponse(jsonObject.Cmd, jsonObject.FirstNum, jsonObject.SecondNum);
+            writer.WriteLine(response);
+            writer.Flush();
+        }
+        socket.Close();
+    }
+
+    static string ServerMethodResponse(string cmd, int firstNum, int secondNum)
+    {
+        string response = "";
+        switch (cmd)
+        {
+            case "Random":
+                Random random = new Random();
+                if (firstNum > secondNum)
+                {
+                    int temp = firstNum;
+                    firstNum = secondNum;
+                    secondNum = temp;
+                }
+                response = "" + random.Next(firstNum, secondNum + 1);
+                break;
+            case "Add":
+                response = "" + (firstNum + secondNum);
+                break;
+            case "Subtract":
+                response = "" + (firstNum - secondNum);
+                break;
+        }
+        return response;
+    }
+}
